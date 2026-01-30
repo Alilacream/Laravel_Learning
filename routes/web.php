0e46e1707bb65b92f0b7e7bc73b11e0de257043d
Alilacream <amaramohammedali42@gmail.com>
2026-01-30 23:41:32 +0100

    Hit rock bottom: Day 3 && Day 4, Unsolvable bug ;-;
    couldn't be able to know why is the Chirp::create in the ChirpController.php file cannot be passed threw the query selector, meanwhile this bum ahh bootcamp shows everything goes water fluide

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,7 @@ use Database\Seeders\ChirpSeeder;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\ChirpController;
-
+// posting the index data
 Route::get('/', [ChirpController::class, 'index']);
+//post the new chirp created
+Route::post('/chirps', [ChirpController::class, 'store']);
