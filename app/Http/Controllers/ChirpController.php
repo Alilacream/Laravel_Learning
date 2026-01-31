e61fe9cc771b530e97698e228c353f89040d4fc7
Alilacream <amaramohammedali42@gmail.com>
2026-01-31 17:30:58 +0100

    Fixed the Day3 && Day4 Bug
    Just found out in my migration table it is actually not an actual database, but we could say a Pipleline to test if the Data insertion is compatable or not, took my  2 days to verify and learn about the  controllers and models, yet the only one who was stopping me is a simple test case :sob:

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -28,16 +28,22 @@ class ChirpController extends Controller
     /**
      * Store a newly created resource in storage.
      */
+
 public function store(Request $request)
 {
     $validated = $request->validate([
-        'message' => 'required|string|max:500',
+        'message' => 'required|string|max:255',
+    ], [
+        'message.required' => 'Please write something to chirp!',
+        'message.max' => 'Chirps must be 255 characters or less.',
     ]);
 
     Chirp::create([
         'message' => $validated['message'],
+        'user_id' => null,
     ]);
-    return redirect('/chirps')->with('success', 'Chirp Created!');
+
+    return redirect('/')->with('success', 'Your chirp has been posted!');
 } 
 
     /**
